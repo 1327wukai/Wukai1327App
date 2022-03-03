1d31038b630aaf715f01bd2f5ba8cd179b561dac
27455 <27455@DESKTOP-2MH1I6N>
2022-03-03 20:30:49 +0800

    添加项目文件。

---
+++ W1HW3App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYearFinder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine("{0} is a Leap year", year);
+            }
+            else
+                Console.WriteLine("{0} is not a Leap year", year);
+            Console.ReadKey();
+        }
+    }
+}
